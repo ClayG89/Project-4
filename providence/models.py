476c6d50eca940186b0ba92eec518fec59f48a31
ClayGuess <guessclay@Yahoo.com>
2020-03-31 17:43:25 -0400

    models.py

--- providence/models.py
+++ providence/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Truck(models.Model):
+    name = models.CharField(max_length=100)
+    trailertype = models.CharField(max_length=100)
+    hours = models.IntegerField()
+    Phone = models.PhoneField()
