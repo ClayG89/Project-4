fdcf40f43520f2138bda2ed0ae785e363322d5d4
ClayGuess <guessclay@Yahoo.com>
2020-03-31 20:50:25 -0400

    created models

--- providence/models.py
+++ providence/models.py
@@ -1,7 +1,35 @@
 from django.db import models
+from phone_field import PhoneField
+
 
 class Truck(models.Model):
     name = models.CharField(max_length=100)
     trailertype = models.CharField(max_length=100)
+    trailernum = models.IntegerField()
     hours = models.IntegerField()
-    Phone = models.PhoneField()
+    Phone = PhoneField(blank=True, help_text='Contact phone number')
+    trucknum = models.IntegerField()
+    drivernum = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Load(models.Model):
+    loadnum = models.IntegerField()
+    pickupnum = models.IntegerField()
+    deliverynum = models.IntegerField()
+    pickuptime = models.DateTimeField()
+    deliverytime = models.DateTimeField()
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+    miles = models.IntegerField()
+    pickuploc = models.CharField(max_length=100)
+    deliveryloc = models.CharField(max_length=100)
+    droppick= models.BooleanField(default=False)
+    dropdel= models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+
