c30d107f36ed373bbc2ec08cdb0c257d2e86ed1d
ClayGuess <guessclay@Yahoo.com>
2020-04-01 17:00:39 -0400

    changed settings.py

--- providence_django/settings.py
+++ providence_django/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    rest_framework
+    'rest_framework',
     'providence',
     'phone_field',
     'django_extensions',
